9e824a204ba57689c6e318a7cf736effc76d2830
Cameron Blocker <cameronjblocker@gmail.com>
2019-03-20 00:18:23 -0400

    Renamed to MRide, created ticker function, created cl setup.py

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
-# Bus Stop CL Query
+# MRide CL Interface
 
-Queries Ann Arbor's Public bus service (theride.org) for bus information because their website is slow.
+Queries Ann Arbor's Public bus service (theride.org) for bus information because their website is slow.  
+In addition to the Ann Arbor Public Bus, the tool also supports looking up UMich buses now via doublemap.  
 
 This is specifically designed to make accessing my most frequent stops easy, but could easily be adapted.
 
-# Usage
+## Install
 
 ```bash
-$ bus_stop --help
-usage: bus_stop [-h] [-V] [-H | -w | -p | -i stop_id] [-n [t]] [-d]
-                [-r route_id]
+pip install https://github.com/cjblocker/mride
+```
+
+## Usage
+
+```bash
+$ mride --help
+usage: mride [-h] [-V] [-H | -w | -p | -i stop_id] [-n [t]] [-d] [-t]
+             [-r route_id]
 
 CL application for checking Ann Arbor Public Bus and UMich Bus info
 
@@ -19,18 +26,29 @@ optional arguments:
   -H, --home            Access Tuebingen & Lancashire Stop information
   -w, --work            Access Beal & Hayward Stop information. Default if no
                         stop option given.
-  -p, --pierpont        Access Pierpont Commons Stop information
+  -p, --pierpont        Access Pierpont Commons Public Stop information
   -i stop_id, --id stop_id
-                        Access the information for the stop with given id
+                        Access the information for the stop with given id.
+                        Note 98 & 101 are both UMich stops near Pierpont, and
+                        137 & 138 are CC Little stops.
   -n [t], --notify [t]  Notify when the bus is t minutes away. Default t=6
   -d, --display         Display Stop information. Default if --notify option
-                        not specified
+                        not specified. If --notify option present, this will
+                        make it verbose.
+  -t, --ticker          Display Stop information, updating every 30s. Cannot
+                        be used with --notify or --display.
   -r route_id, --route route_id
-                        The route number to return information on. Default = 22
+                        The route number to return information on. Options
+                        include BB for Bursely-Baits, 22, 65, CN for Commuter
+                        North, etc. Default = *
 ```
 
-# Future Work
+## Future Work
 
 - Multiple stops at once
 - flag configuration file
+- Easier access to UMich information
+- UMich stop number lookup by name/code
+- terminal colors? i.e. red for soon, or by route?
+- Pop-Up or other notifications for not Mac computers
 
--- mride.py
+++ mride.py
@@ -6,36 +6,39 @@ import time
 import argparse
 import os
 import functools
+import curses
 
 __VERSION__ = '0.1'
 
-BUS_API_ENDPOINT='http://www.theride.org/DesktopModules/AATA.EndPoint/Proxy.ashx?method=getpredictionsfromxml&stpid={}'
+MRIDE_BUS_API_ENDPOINT='http://www.theride.org/DesktopModules/AATA.EndPoint/Proxy.ashx?method=getpredictionsfromxml&stpid={}'
 UMICH_BUS_API_ENDPOINT='http://mbus.doublemap.com/map/v2/eta?stop={}' #['etas']['stopID']['etas']
 UMICH_BUS_API_BUS_LIST='http://mbus.doublemap.com/map/v2/routes?id={}' # list all bus routes with name, id, stops, active
 UMICH_BUS_API_STOP_LIST='http://mbus.doublemap.com/map/v2/stops?id={}' # get a stop name/code from id
 
-
+# MRide Stop IDs
 WORK_STOP_ID = 1550
 HOME_STOP_ID = 1781
 PIERPONT_STOP_ID = 1714
 
-# UMich Buses via Magic Bus
-BURSLEY_BAITS = 1032
-COMMUTER_NORTH = 1051
-COMMUTER_SOUTH = 1049
-NORTHWOOD = 1055
+# UMich Routes via Magic Bus
+BURSLEY_BAITS = 'BB'
+COMMUTER_NORTH = 'CN'
+COMMUTER_SOUTH = 'CS'
+NORTHWOOD = 'NW'
+NORTHWOOD_EXPRESS = 'NWx'
+DIAG_2_DIAG = 'D2D'
 
 # UMich Stop IDs
 N551_ID =  101 # BB Outbound from pierpont
 N553_ID = 98   # Outside pierpont
-C250_ID = 137 # CC little chem side
-C251_ID = 138 # CC little museum side
+C250_ID = 137  # CC little chem side
+C251_ID = 138  # CC little museum side
 
 def mac_pop_up(title, text):
     os.system("""osascript -e 'display notification "{}" with title "{}"'
               """.format(text, title))
 
-@functools.lru_cache()
+@functools.lru_cache() # This info is static
 def get_umich_route_name_from_id(route_id):
     try:
         route_info = requests.get(UMICH_BUS_API_BUS_LIST.format(route_id)).json()
@@ -48,6 +51,7 @@ def get_umich_bus_stop_info(stop_id):
         stop_loc = requests.get(UMICH_BUS_API_ENDPOINT.format(stop_id)).json()['etas'][str(stop_id)]['etas']
     except KeyError:
         print('Currently No Buses for that Stop')
+        return []
 
     if not isinstance(stop_loc, list): stop_loc = [stop_loc]
 
@@ -60,16 +64,15 @@ def get_umich_bus_stop_info(stop_id):
             'route': bus['route'],
             'eta': bus['avg'],
             })
-    # print(stop_info)
     return stop_info 
 
 def get_mride_bus_stop_info(stop_id):
     # print(requests.get(BUS_API_ENDPOINT.format(stop_id)).json())
     try:
-        stop_loc = requests.get(BUS_API_ENDPOINT.format(stop_id)).json()['bustime-response']['prd']
+        stop_loc = requests.get(MRIDE_BUS_API_ENDPOINT.format(stop_id)).json()['bustime-response']['prd']
     except KeyError:
         print('Currently No Buses for that Stop')
-        return
+        return []
 
     if not isinstance(stop_loc, list): stop_loc = [stop_loc]
 
@@ -79,12 +82,12 @@ def get_mride_bus_stop_info(stop_id):
             'name': bus['des'],
             'short_name': bus['rt'],
             'route': bus['rt'],
-            'eta': bus['prdctdn'],
+            'eta': bus['prdctdn'] if bus['prdctdn'] != 'DUE' else 0,
             })
     return stop_info 
 
 def is_mride_id(stop_id):
-    return stop_id > 999
+    return stop_id > 999 # This seems sufficient
 
 def _get_stop_info(stop_id, route_id='*'):
     if is_mride_id(stop_id):
@@ -101,28 +104,48 @@ def _get_stop_info(stop_id, route_id='*'):
     return stop_info
 
 
-def setup_notify(stop_id, route_id, minutes):
+def setup_notify(stop_id, route_id, minutes, display=False):
     stop_loc = _get_stop_info(stop_id, route_id)
     time_left = min([int(bus['eta']) for bus in stop_loc])
-    print(f"time_left = {time_left}")
+    if display: print_bus_info([bus for bus in stop_loc if int(bus['eta']) == time_left][0], prepend='\r', end="")
     while time_left > minutes:
         time.sleep(60*(time_left - minutes)/2) # wait half expected amount
+        # time.sleep(10)
         stop_loc = _get_stop_info(stop_id, route_id)
         time_left = min([int(bus['eta']) for bus in stop_loc])
-        print(f"time_left = {time_left}")
+        if display: print_bus_info([bus for bus in stop_loc if int(bus['eta']) == time_left][0], prepend='\r', end="")
+    if display: print('')
     mac_pop_up("Bus Stop", "Time to go, bus will be here in {} minutes".format(time_left))
 
 def display_stop_info(stop_id, route_id='sort'):
     stop_loc = _get_stop_info(stop_id, route_id)
+    for bus in stop_loc:
+            print_bus_info(bus)
 
+# @curses.wrapper
+def ticker(stop_id, route_id):
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    stdscr.clear()
     try:
-        for bus in stop_loc:
-            print("{:<25} {:>10}min  {:%I:%M %p}".format(bus['name'].strip(),bus['eta'], dt.now()+timedelta(minutes=int(bus['eta']))))
-    except ValueError:
-        print(stop_loc)
+        while(1):
+            stop_loc = _get_stop_info(stop_id, route_id)
+            for ii, bus in enumerate(stop_loc):
+                stdscr.addstr(ii,0, "{:<3}| {:<30} | {:>2} min | {:%I:%M %p}".format(bus['short_name'],bus['name'].strip()[:30],bus['eta'], dt.now()+timedelta(minutes=int(bus['eta']))))
+            stdscr.refresh()
+            time.sleep(30)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        curses.nocbreak()
+        stdscr.keypad(False)
+        curses.echo()
+        curses.endwin()
+    # print('')
 
-def install():
-    pass
+def print_bus_info(bus, prepend='', end='\n'):
+    print("{}{:<3}| {:<30} | {:>2} min | {:%I:%M %p}".format(prepend, bus['short_name'],bus['name'].strip()[:30],bus['eta'], dt.now()+timedelta(minutes=int(bus['eta']))), end=end)
 
 def parse_arguments():
 
@@ -135,16 +158,22 @@ def parse_arguments():
     stop_group.add_argument('-w','--work', action="store_true",
                         help='Access Beal & Hayward Stop information. Default if no stop option given.')
     stop_group.add_argument('-p','--pierpont', action="store_true",
-                        help='Access Pierpont Commons Stop information')
+                        help='Access Pierpont Commons Public Stop information')
     stop_group.add_argument('-i','--id', type=int, metavar='stop_id',
-                        help='Access the information for the stop with given id')
+                        help='Access the information for the stop with given id. \
+                        Note 98 & 101 are both UMich stops near Pierpont, and 137 & 138 are CC Little stops.')
 
     parser.add_argument('-n','--notify', type=int, nargs='?', const=6, default=0, metavar='t',
                         help='Notify when the bus is t minutes away. Default t=6')
     parser.add_argument('-d','--display', action="store_true",
-                        help='Display Stop information. Default if --notify option not specified')
-    parser.add_argument('-r','--route', type=str, default='22', metavar='route_id',
-                        help='The route number to return information on. Default = 22')
+                        help='Display Stop information. Default if --notify option not specified. \
+                        If --notify option present, this will make it verbose.')
+    parser.add_argument('-t','--ticker', action="store_true",
+                        help='Display Stop information, updating every 30s. Cannot be used with \
+                        --notify or --display.')
+    parser.add_argument('-r','--route', type=str, default='*', metavar='route_id',
+                        help='The route number to return information on. Options include BB for \
+                        Bursely-Baits, 22, 65, CN for Commuter North, etc. Default = *')
 
 
     args = parser.parse_args()
@@ -161,13 +190,16 @@ def parse_arguments():
     if not args.notify:
         args.display = True # Default
 
-    return stop_id, args.route, args.display, args.notify    
+    return stop_id, args.route, args.display, args.notify, args.ticker    
 
-def main(stop_id=WORK_STOP_ID, route_id=22, do_print=True, notify_minutes=0):
-    if do_print:
-        display_stop_info(stop_id, route_id)
-    if notify_minutes:
-        setup_notify(stop_id, route_id, notify_minutes)
+def main(stop_id=WORK_STOP_ID, route_id=22, do_print=True, notify_minutes=0, do_ticker=False):
+    if do_ticker:
+        ticker(stop_id, route_id)
+    else:
+        if do_print and not notify_minutes:
+            display_stop_info(stop_id, route_id)
+        if notify_minutes:
+            setup_notify(stop_id, route_id, notify_minutes, display=do_print)
 
 def run():
     main(*parse_arguments())
